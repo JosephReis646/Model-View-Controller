8871c20b0b5a9275097debf85e02c2b7f283741b
joseph reis <josephreis646@gmail.com>
2023-03-21 20:43:06 -0400

    Create and update a post

--- controllers/postController.js
+++ controllers/postController.js
@@ -31,3 +31,35 @@ router.get('/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // Create a new post
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+      res.status(200).json(postData);
+    } catch (err) {
+    res.status(400).json(err);
+    }
+    });
+    
+    // Update a post
+    router.put('/:id', withAuth, async (req, res) => {
+    try {
+    const postData = await Post.update(req.body, {
+    where: {
+    id: req.params.id,
+    },
+    });
+    if (!postData) {
+        res.status(404).json({ message: 'No post found with that id!' });
+        return;
+      }
+      
+      res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+        }
+        });      

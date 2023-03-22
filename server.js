143fbbd94cdec347c793f263e0e4884fc44fd07f
joseph reis <josephreis646@gmail.com>
2023-03-21 20:24:56 -0400

    Handlebar.js added to server.js

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Load environment variables
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({});

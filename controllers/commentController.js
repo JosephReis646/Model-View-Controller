baf43e2578dad7f21a2cdb156e0a3dd8494da8c1
joseph reis <josephreis646@gmail.com>
2023-03-21 20:45:02 -0400

    Create a new comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -27,3 +27,18 @@ router.get('/post/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // Create a new comment
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  module.exports = router;

a85af7737534734e2d96ce8d8e140a4da86dbff9
joseph reis <josephreis646@gmail.com>
2023-03-21 20:39:41 -0400

    Sign up a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,3 +11,17 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Sign up a new user
+router.post('/signup', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });

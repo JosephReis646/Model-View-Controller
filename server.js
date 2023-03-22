59e27a02e454ed6c66953ffb83ae167b4c4a189c
joseph reis <josephreis646@gmail.com>
2023-03-21 20:23:30 -0400

    Import dependencies to server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Import necessary dependencies
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+

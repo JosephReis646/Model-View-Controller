cedf46e00f26758d97de3f4217f21c372febf546
joseph reis <josephreis646@gmail.com>
2023-03-21 20:39:19 -0400

    Get all users in usercontrollers

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});

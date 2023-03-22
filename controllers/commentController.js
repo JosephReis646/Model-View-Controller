6b39de295bb32f998902db5e992c2c0206766d2a
joseph reis <josephreis646@gmail.com>
2023-03-21 20:44:40 -0400

    get a specific comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -13,3 +13,17 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Get comments for a specific post
+router.get('/post/:id', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll({
+        where: { post_id: req.params.id },
+        include: [{ model: User, attributes: ['username'] }],
+      });
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
